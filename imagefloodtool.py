51ad367371afc1d434cf8ad63a587258431f0e2b
Luiz Motta <motta.luiz@gmail.com>
2021-07-25 15:03:03 -0300

    Check raster and split imagefllodtool

--- imagefloodtool.py
+++ imagefloodtool.py
@@ -1,215 +1,26 @@
 
-from plugin.geometry import Polygon
-from qgis.PyQt.QtCore import Qt, QByteArray, QBuffer, QIODevice
-from qgis.PyQt.QtGui import QImage, QColor
+from qgis.PyQt.QtCore import Qt
 from qgis.PyQt.QtWidgets import QLabel, QFrame, QMessageBox
 
 from qgis.core import (
     QgsProject,
     QgsMapLayer, QgsVectorLayer, QgsRasterLayer,
     QgsGeometry, QgsFeature, QgsWkbTypes,
-    QgsMapSettings, 
-    QgsCoordinateReferenceSystem, QgsCoordinateTransform,
-    QgsMapRendererParallelJob 
+    QgsCoordinateReferenceSystem, QgsCoordinateTransform
 )
-from qgis.gui import QgsMapTool, QgsMapCanvasItem 
+from qgis.gui import QgsMapTool
 
-from osgeo import gdal, gdal_array, ogr, osr
+from osgeo import gdal, ogr
 
-from PIL import Image, ImageDraw
-import numpy as np
 from scipy import ndimage
 
 import os
 
-
-class MapItemFlood(QgsMapCanvasItem):
-    def __init__(self, mapCanvas):
-        super().__init__( mapCanvas )
-        self.mapCanvas = mapCanvas
-        self.layers = []
-        self.enabled = True
-
-    def paint(self, painter, *args): # NEED *args for   WINDOWS!
-        def finished():
-            image = job.renderedImage()
-            if bool( self.mapCanvas.property('retro') ):
-                image = image.scaled( image.width() / 3, image.height() / 3 )
-                image = image.convertToFormat( QImage.Format_Indexed8, Qt.OrderedDither | Qt.OrderedAlphaDither )
-            painter.drawImage( image.rect(), image )
-
-        if not self.enabled or len( self.layers ) == 0:
-            return
-
-        settings = QgsMapSettings( self.mapCanvas.mapSettings() )
-        settings.setLayers( self.layers )
-        settings.setBackgroundColor( QColor( Qt.transparent ) )
-        
-        self.setRect( self.mapCanvas.extent() )
-        job = QgsMapRendererParallelJob( settings ) 
-        job.start()
-        job.finished.connect( finished) 
-        job.waitForFinished()
-        
-    def setLayers(self, layers):
-        self.layers = layers
-
-
-class ImageCanvas():
-    def __init__(self, canvas):
-        self.root = QgsProject.instance().layerTreeRoot()
-        self.mapCanvas = canvas
-        #
-        self.dataset = None # set by process.finished
-        self.extent = None
-        self.rasters = None
-
-    def _rastersCanvas(self):
-        return [ l for l in self.root.checkedLayers() if not l is None and l.type() == QgsMapLayer.RasterLayer ]
-
-    def process(self):
-        def finished():
-            def createDataset(image):
-                def setGeoreference(ds):
-                    #
-                    extent = self.mapCanvas.extent()
-                    imgWidth, imgHeight = image.width(), image.height()
-                    resX, resY = extent.width() / imgWidth, extent.height() / imgHeight
-                    geoTrans = ( extent.xMinimum(), resX, 0.0, extent.yMaximum(), 0.0, -1 * resY )
-                    ds.SetGeoTransform( geoTrans )
-                    # 
-                    crs = self.mapCanvas.mapSettings().destinationCrs()
-                    srs = osr.SpatialReference()
-                    srs.ImportFromWkt( crs.toWkt() )
-                    ds.SetSpatialRef( srs )
-                # Copy image to QByteArray
-                ba = QByteArray()
-                buf = QBuffer( ba )
-                buf.open( QIODevice.WriteOnly )
-                image.save( buf, "TIFF", 100 )
-                buf.close()
-                # Create Dataset
-                filename = '/vsimem/mem.tif'
-                gdal.FileFromMemBuffer( filename, ba.data() )
-                ds = gdal.Open( filename )
-                ds_mem = gdal.GetDriverByName('MEM').CreateCopy( '', ds )
-                setGeoreference( ds_mem )
-                ds = None
-                gdal.Unlink( filename )
-                ba = None
-                #
-                return ds_mem
-
-            image = job.renderedImage()
-            if bool( self.mapCanvas.property('retro') ):
-                image = image.scaled( image.width() / 3, image.height() / 3 )
-                image = image.convertToFormat( QImage.Format_Indexed8, Qt.OrderedDither | Qt.OrderedAlphaDither )
-
-            self.dataset = createDataset( image )
-
-        self.dataset = None
-        rasters = self._rastersCanvas()
-        if not len( rasters ):
-            return
-        self.extent = self.mapCanvas.extent()
-        self.rasters = rasters
-        
-        settings = QgsMapSettings( self.mapCanvas.mapSettings() )
-        settings.setBackgroundColor( QColor( Qt.transparent ) )
-        settings.setLayers( rasters )
-        job = QgsMapRendererParallelJob( settings ) 
-        job.start()
-        job.finished.connect( finished) 
-        job.waitForFinished()
-
-    def changedCanvas(self):
-        return not ( self.rasters == self._rastersCanvas() and self.extent == self.mapCanvas.extent() )
-
-
-class CalculateArrayFlood():
-    def __init__(self):
-        self.flood_value = None # Calculate when read image
-        self.flood_value_color = 255
-        self.flood_out = 0
-        self.threshFlood = 10 # 0 .. 255
-        self.threshSieve = 100
-
-    def get(self, arraySource, seed, threshFlood=None):
-        as_image = (1,2,0)  # rows, columns, bands
-        as_raster = (2,0,1) # bands, rows, columns
-        #
-        n_bands = arraySource.shape[0]
-        arry = np.transpose( arraySource, as_image )
-        img_flood = Image.fromarray( arry )
-        l_flood_value = tuple( n_bands * [ self.flood_value ] )
-        tf = threshFlood if threshFlood else self.threshFlood
-        ImageDraw.floodfill( img_flood, seed, l_flood_value, thresh=tf )
-        # Change outside flood
-        arry = np.array( img_flood )
-        arry = np.transpose( arry, as_raster )
-        bool_out = ~(arry == self.flood_value)
-        arry[ bool_out ] = self.flood_out
-        # Sieve
-        ds = gdal_array.OpenArray( arry ) # Read only
-        dsSieve = gdal.GetDriverByName('MEM').CreateCopy('', ds )
-        ds = None
-        band_sieve = dsSieve.GetRasterBand(1)
-        gdal.SieveFilter( srcBand=band_sieve, maskBand=None, dstBand=band_sieve, threshold=self.threshSieve, connectedness=8 )
-        arry_sieve = band_sieve.ReadAsArray()
-        for b in range( 1, dsSieve.RasterCount ):
-            band = dsSieve.GetRasterBand( b+1 )
-            gdal.SieveFilter( srcBand=band, maskBand=None, dstBand=band, threshold=self.threshSieve, connectedness=8 )
-            arry_band = band.ReadAsArray()
-            bool_s = ( arry_sieve == self.flood_value )
-            bool_b = ( arry_band == self.flood_value )
-            arry_sieve[ bool_s * bool_b ] = self.flood_value
-            arry_sieve[ ~(bool_s * bool_b) ] = self.flood_out
-        dsSieve = None
-
-        arry_sieve[ arry_sieve == self.flood_value ] = self.flood_value_color
-        return arry_sieve
-
-    def getThresholdFlood(self, point1, point2):
-        minDelta = 1
-        maxDelta = 254
-        delta = point2 - point1
-        dX, dY = delta.x(), delta.y()
-        delta = dX if abs( dX) > abs( dY ) else -1*dY
-        threshFlood = self.threshFlood + delta
-        if threshFlood < minDelta:
-            threshFlood = minDelta
-        if threshFlood > maxDelta:
-            threshFlood = maxDelta
-        return threshFlood
-
-
-def createDatasetMem(arry, geoTransform, spatialRef, nodata=None):
-    if len( arry.shape ) == 2:
-        rows, columns = arry.shape
-        bands = 1
-    else:
-        bands, rows, columns = arry.shape
-    data_type = gdal_array.NumericTypeCodeToGDALTypeCode( arry.dtype )
-    ds = gdal.GetDriverByName('MEM').Create('', columns, rows, bands, data_type )
-    if bands == 1:
-        band = ds.GetRasterBand(1)
-        band.WriteArray( arry )
-        if not nodata is None:
-            band.SetNoDataValue( nodata )
-    else:
-        for b in range( bands ):
-            band = ds.GetRasterBand( b+1 )
-            band.WriteArray( arry[ b ] )
-            if not nodata is None:
-                band.SetNoDataValue( nodata )
-    ds.SetGeoTransform( geoTransform )
-    ds.SetSpatialRef( spatialRef )
-    return ds
-
+from .utils import MapItemFlood, CanvasImage, CalculateArrayFlood, createDatasetMem
 
 class ImageFlood():
     def __init__(self, mapCanvas):
-        self.canvasImage = ImageCanvas( mapCanvas )
+        self.canvasImage = CanvasImage( mapCanvas )
         self.mapItem = MapItemFlood( mapCanvas )
         self.calcFlood = CalculateArrayFlood()
 
@@ -236,6 +47,9 @@ class ImageFlood():
     def setLayerSeed(self, pointMap):
         self.lyrSeed = self._createQgsSeedVector( pointMap )
 
+    def existsRasterLayer(self):
+        return len( self.canvasImage.rasterLayers() ) > 0
+
     def changedCanvas(self):
         return self.canvasImage.changedCanvas()
 
@@ -255,7 +69,8 @@ class ImageFlood():
         self.canvasImage.process()
         if not self.canvasImage.dataset:
             raise TypeError("Error created image from canvas. Check exists raster layer visible")
-        self.calcFlood.flood_value = getFloodValue()
+        if not self.calcFlood.setFloodValue( self.canvasImage.dataset ):
+            raise TypeError("Impossible define value of seed")
 
     def calculateThreshold(self, point1, point2):
         return self.calcFlood.getThresholdFlood( point1, point2 )
@@ -455,6 +270,7 @@ class ImageFloodTool(QgsMapTool):
 
         self.imageFlood = ImageFlood( self.mapCanvas )
         
+        self.existsRasterLayer = False
         self.hasPressPoint = False
         self.threshFloodMove = None
 
@@ -486,6 +302,10 @@ class ImageFloodTool(QgsMapTool):
             self.imageFlood.enabledFloodCanvas( False )
             return
 
+        self.existsRasterLayer = self.imageFlood.existsRasterLayer()
+        if not self.existsRasterLayer:
+            return
+
         self.hasPressPoint = True
         
         self.threshFloodMove = None
@@ -498,7 +318,9 @@ class ImageFloodTool(QgsMapTool):
             self.hasPressPoint = False # Escape canvasMoveEvent
 
     def canvasMoveEvent(self, e):
-        if not self.hasPressPoint: # Always e.button() = 0
+        # Always e.button() = 0
+
+        if not self.hasPressPoint or not self.existsRasterLayer:
             return
 
         if self.imageFlood.changedCanvas():
@@ -512,6 +334,11 @@ class ImageFloodTool(QgsMapTool):
             self.threshFloodMove = None
 
     def canvasReleaseEvent(self, e):
+        if not self.existsRasterLayer:
+            msg = 'Missing raster layer visible in Map'
+            self.msgBar.pushWarning( self.PLUGINNAME, msg )
+            return
+
         self.hasPressPoint = False
         if e.button() == Qt.RightButton:
             self.imageFlood.enabledFloodCanvas()
@@ -646,15 +473,9 @@ class ImageFloodTool(QgsMapTool):
         self.statusBar.addPermanentWidget( self.lblThreshFlood, 0)
         self._setTextTreshold( self.imageFlood.getCurrentThreshold() )
 
-        # if self.layerFlood is None:
-        #     self.layerFlood = QgsVectorLayer( 'Polygon?crs=EPSG:4326', 'flood', 'memory')
-        #     self.project.addMapLayer( self.layerFlood )
-        #     self.layerFlood.loadNamedStyle( self.stylePoylgon )
-
     def _deactivatedTool(self):
         self.statusBar.removeWidget( self.lblMessageFlood )
         self.statusBar.removeWidget( self.lblThreshFlood )
-
 
 
 
---
+++ utils.py
@@ -0,0 +1,226 @@
+
+from qgis.PyQt.QtCore import Qt, QByteArray, QBuffer, QIODevice
+from qgis.PyQt.QtGui import QImage, QColor
+
+from qgis.core import (
+    QgsProject,
+    QgsMapLayer,
+    QgsMapSettings, 
+    QgsMapRendererParallelJob,
+    QgsCoordinateTransform 
+)
+from qgis.gui import QgsMapCanvasItem 
+
+from osgeo import gdal, gdal_array, osr
+
+from PIL import Image, ImageDraw
+import numpy as np
+
+
+class MapItemFlood(QgsMapCanvasItem):
+    def __init__(self, mapCanvas):
+        super().__init__( mapCanvas )
+        self.mapCanvas = mapCanvas
+        self.layers = []
+        self.enabled = True
+
+    def paint(self, painter, *args): # NEED *args for   WINDOWS!
+        def finished():
+            image = job.renderedImage()
+            if bool( self.mapCanvas.property('retro') ):
+                image = image.scaled( image.width() / 3, image.height() / 3 )
+                image = image.convertToFormat( QImage.Format_Indexed8, Qt.OrderedDither | Qt.OrderedAlphaDither )
+            painter.drawImage( image.rect(), image )
+
+        if not self.enabled or len( self.layers ) == 0:
+            return
+
+        settings = QgsMapSettings( self.mapCanvas.mapSettings() )
+        settings.setLayers( self.layers )
+        settings.setBackgroundColor( QColor( Qt.transparent ) )
+        
+        self.setRect( self.mapCanvas.extent() )
+        job = QgsMapRendererParallelJob( settings ) 
+        job.start()
+        job.finished.connect( finished) 
+        job.waitForFinished()
+        
+    def setLayers(self, layers):
+        self.layers = layers
+
+
+class CanvasImage():
+    def __init__(self, canvas):
+        self.project = QgsProject.instance()
+        self.root = self.project.layerTreeRoot()
+        self.mapCanvas = canvas
+        # Set by process
+        self.extent = None
+        self.rasters = None
+        self.dataset = None # set by process.finished
+
+    def rasterLayers(self):
+        layers =  [ l for l in self.root.checkedLayers() if not l is None and l.type() == QgsMapLayer.RasterLayer ]
+        if not len( layers ):
+            return []
+
+        # Check Intersects
+        extent = self.mapCanvas.extent()
+        ct = QgsCoordinateTransform()
+        ct.setDestinationCrs( self.project.crs() )
+        layersCanvas = []
+        for layer in layers:
+            crsLayer = layer.crs()
+            ct.setSourceCrs( crsLayer )
+            extentLayer = layer.extent()
+            extentLayer = ct.transform( extentLayer )
+            if extent.intersects( extentLayer ):
+                layersCanvas.append( layer )
+        return layersCanvas
+
+    def process(self):
+        def finished():
+            def createDataset(image):
+                def setGeoreference(ds):
+                    #
+                    extent = self.mapCanvas.extent()
+                    imgWidth, imgHeight = image.width(), image.height()
+                    resX, resY = extent.width() / imgWidth, extent.height() / imgHeight
+                    geoTrans = ( extent.xMinimum(), resX, 0.0, extent.yMaximum(), 0.0, -1 * resY )
+                    ds.SetGeoTransform( geoTrans )
+                    # 
+                    crs = self.mapCanvas.mapSettings().destinationCrs()
+                    srs = osr.SpatialReference()
+                    srs.ImportFromWkt( crs.toWkt() )
+                    ds.SetSpatialRef( srs )
+                # Copy image to QByteArray
+                ba = QByteArray()
+                buf = QBuffer( ba )
+                buf.open( QIODevice.WriteOnly )
+                image.save( buf, "TIFF", 100 )
+                buf.close()
+                # Create Dataset
+                filename = '/vsimem/mem.tif'
+                gdal.FileFromMemBuffer( filename, ba.data() )
+                ds = gdal.Open( filename )
+                ds_mem = gdal.GetDriverByName('MEM').CreateCopy( '', ds )
+                setGeoreference( ds_mem )
+                ds = None
+                gdal.Unlink( filename )
+                ba = None
+                #
+                return ds_mem
+
+            image = job.renderedImage()
+            if bool( self.mapCanvas.property('retro') ):
+                image = image.scaled( image.width() / 3, image.height() / 3 )
+                image = image.convertToFormat( QImage.Format_Indexed8, Qt.OrderedDither | Qt.OrderedAlphaDither )
+
+            self.dataset = createDataset( image )
+
+        self.dataset = None
+        rasters = self.rasterLayers()
+        if not len( rasters ):
+            return
+        self.extent = self.mapCanvas.extent()
+        self.rasters = rasters
+        
+        settings = QgsMapSettings( self.mapCanvas.mapSettings() )
+        settings.setBackgroundColor( QColor( Qt.transparent ) )
+        settings.setLayers( rasters )
+        job = QgsMapRendererParallelJob( settings ) 
+        job.start()
+        job.finished.connect( finished) 
+        job.waitForFinished()
+
+    def changedCanvas(self):
+        return not ( self.rasters == self.rasterLayers() and self.extent == self.mapCanvas.extent() )
+
+
+class CalculateArrayFlood():
+    def __init__(self):
+        self.flood_value = None
+        self.flood_value_color = 255
+        self.flood_out = 0
+        self.threshFlood = 10 # 0 .. 255
+        self.threshSieve = 100
+
+    def get(self, arraySource, seed, threshFlood=None):
+        as_image = (1,2,0)  # rows, columns, bands
+        as_raster = (2,0,1) # bands, rows, columns
+        #
+        n_bands = arraySource.shape[0]
+        arry = np.transpose( arraySource, as_image )
+        img_flood = Image.fromarray( arry )
+        l_flood_value = tuple( n_bands * [ self.flood_value ] )
+        tf = threshFlood if threshFlood else self.threshFlood
+        ImageDraw.floodfill( img_flood, seed, l_flood_value, thresh=tf )
+        # Change outside flood
+        arry = np.array( img_flood )
+        arry = np.transpose( arry, as_raster )
+        bool_out = ~(arry == self.flood_value)
+        arry[ bool_out ] = self.flood_out
+        # Sieve
+        ds = gdal_array.OpenArray( arry ) # Read only
+        dsSieve = gdal.GetDriverByName('MEM').CreateCopy('', ds )
+        ds = None
+        band_sieve = dsSieve.GetRasterBand(1)
+        gdal.SieveFilter( srcBand=band_sieve, maskBand=None, dstBand=band_sieve, threshold=self.threshSieve, connectedness=8 )
+        arry_sieve = band_sieve.ReadAsArray()
+        for b in range( 1, dsSieve.RasterCount ):
+            band = dsSieve.GetRasterBand( b+1 )
+            gdal.SieveFilter( srcBand=band, maskBand=None, dstBand=band, threshold=self.threshSieve, connectedness=8 )
+            arry_band = band.ReadAsArray()
+            bool_s = ( arry_sieve == self.flood_value )
+            bool_b = ( arry_band == self.flood_value )
+            arry_sieve[ bool_s * bool_b ] = self.flood_value
+            arry_sieve[ ~(bool_s * bool_b) ] = self.flood_out
+        dsSieve = None
+
+        arry_sieve[ arry_sieve == self.flood_value ] = self.flood_value_color
+        return arry_sieve
+
+    def getThresholdFlood(self, point1, point2):
+        minDelta = 1
+        maxDelta = 254
+        delta = point2 - point1
+        dX, dY = delta.x(), delta.y()
+        delta = dX if abs( dX) > abs( dY ) else -1*dY
+        threshFlood = self.threshFlood + delta
+        if threshFlood < minDelta:
+            threshFlood = minDelta
+        if threshFlood > maxDelta:
+            threshFlood = maxDelta
+        return threshFlood
+
+    def setFloodValue(self, dsImage):
+        arry = dsImage.ReadAsArray()
+        for v in range(1, 256):
+            if arry[arry == v].sum() == 0:
+                self.flood_value = v
+                return True
+        return False
+
+
+def createDatasetMem(arry, geoTransform, spatialRef, nodata=None):
+    if len( arry.shape ) == 2:
+        rows, columns = arry.shape
+        bands = 1
+    else:
+        bands, rows, columns = arry.shape
+    data_type = gdal_array.NumericTypeCodeToGDALTypeCode( arry.dtype )
+    ds = gdal.GetDriverByName('MEM').Create('', columns, rows, bands, data_type )
+    if bands == 1:
+        band = ds.GetRasterBand(1)
+        band.WriteArray( arry )
+        if not nodata is None:
+            band.SetNoDataValue( nodata )
+    else:
+        for b in range( bands ):
+            band = ds.GetRasterBand( b+1 )
+            band.WriteArray( arry[ b ] )
+            if not nodata is None:
+                band.SetNoDataValue( nodata )
+    ds.SetGeoTransform( geoTransform )
+    ds.SetSpatialRef( spatialRef )
+    return ds
